20ee757e6c82e95f387988ec7479f2d37c593610
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-06-12 20:02:15 +0200

    Added script to creata and configure a new interface

---
+++ rosToCMD/create_interface.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Get the arguments
+packagePath=$1
+interfaceName=$2
+interfaceType=$3
+
+# Check the arguments
+if [ -z "$packagePath" ]; then
+	echo "ERROR: You have to provide the path to the package"
+	exit 1
+fi
+if [ -z "$interfaceName" ]; then
+	echo "ERROR: You have to provide a name for the new interface"
+	exit 2
+fi
+if [ -z "$interfaceType" ]; then
+	echo "ERROR: You have to provide an interface type (msg/srv/action)"
+	exit 3
+fi
+if [ ! -d "$packagePath" ]; then
+	echo "ERROR: Unable to locate the given package"
+	exit 4
+fi
+if [[ "$interfaceType" != "msg" && "$interfaceType" != "svg" && "$interfaceType" != "action" ]]; then
+	echo "ERROR: Unsupportet interface type"
+	exit 5
+fi
+
+# Generate a new file with the correct ending
+cd "${packagePath}/${interfaceType}"
+touch "${interfaceName}.${interfaceType}"
+if [ ! -f "${interfaceName}.${interfaceType}" ]; then
+		echo "ERROR: Unable to locate the new interface file"
+		exit 6
+fi
+
+# Receive the interface schema
+nano "${interfaceName}.${interfaceType}"
+
+# Make sure the CMakeLists.txt exists
+cd "$packagePath"
+if [ ! -f "CMakeLists.txt" ]; then
+	echo "ERROR: Unable to find the CMakeLists.txt file"
+	exit 7
+fi
+
+# Create the entry and share it with perl
+entry="  ${interfaceType}/${interfaceName}.${interfaceType}"
+export ENTRY="$entry"
+
+# Write the entry into the CMakeLists.txt
+perl -0777 -i.bak -pe 's/(rosidl_generate_interfaces\(.*?)\n(\s*\))/ $1 . "\n" . $ENV{ENTRY} . "\n" . $2 /sge' CMakeLists.txt || {
+	echo "ERROR: An error occured while adding the new interface to the CMakeLists.txt"
+	exit 8
+}
+rm "CMakeLists.txt.bak"
+
+# Close with success
+echo "Success"
